b557079a25b4d3323d5fbbc833829b7c75573b9d
Gogi0903 <gogh.jozsef@gmail.com>
2023-09-19 15:33:47 -0400

    MAC changer V1.2

--- mac_changer_V1.2.py
+++ mac_changer_V1.2.py
@@ -4,7 +4,7 @@ import re
 
 
 def change_mac(interface, new_mac):
-
+    # megváltoztatja a definiált interface MAC addressét.
     print(f'[+] A(z) "{interface}" MAC address megváltoztatása erre: {new_mac}')
     subprocess.call(["ifconfig", interface, 'down'])
     subprocess.call(["ifconfig", interface, 'hw', 'ether', new_mac])
@@ -12,24 +12,25 @@ def change_mac(interface, new_mac):
 
 
 def get_arguments():
+    # a kód futtatásakor meghatározott argumentumokat adja vissza
     parser = optparse.OptionParser()
     parser.add_option('-i', '--interface', dest='interface', help='az interface, aminek a MAC address-e változik')
     parser.add_option('-m', '--mac', dest='new_mac', help='ez lesz az új MAC address')
     (option, arguments) = parser.parse_args()
     if not option.interface:
-        # code to handle error
+        # hibakezelés
         parser.error('[-] Adj meg egy interface-t, használd a --help parancsot több infóért.')
     elif not option.new_mac:
-        # code to handle error
+        # hibakezelés
         parser.error('[-] Adj meg egy új MAC address, használd --help parancsot több infóért.')
     return option
 
 
 def get_current_mac(interface):
+    # visszaadja a jelenlegi MAC addresst
     ifconfig_result = subprocess.check_output(["ifconfig", interface])
     pattern = r'[0-9A-Fa-f]{2}(:[0-9A-Fa-f]{2}){5}'
     mac_address_search_result = re.search(pattern, ifconfig_result.decode('utf-8'))
-
     if mac_address_search_result:
         return mac_address_search_result.group(0)
     else:
